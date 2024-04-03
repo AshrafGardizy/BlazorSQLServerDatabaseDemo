4ff82443ddf0ae767cb1572009ddfe9f9aa5c778
ashra <ashra@ASHRAF>
2024-04-03 16:37:46 +0200

    Add project files.

---
+++ BlazorDatabaseDemo/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorDatabaseDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDatabaseDemo.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public IConfiguration iconfig { get; set; }
+        public AppDBContext(IConfiguration config)
+        {
+            iconfig = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(iconfig.GetConnectionString("DatabaseConnection"));
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
